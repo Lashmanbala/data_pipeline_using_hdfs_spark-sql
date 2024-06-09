d6bb4839124bbf93374adcb8789efcdd955e1ed9
Lashmanbala <lashmanbala@gmail.com>
2024-06-09 09:16:04 +0000

    Creating an external table for staging

--- app.sql
+++ app.sql
@@ -1,3 +1,16 @@
 CREATE DATABASE IF NOT EXISTS nyse_db;
 
 USE nyse_db;
+
+CREATE EXTERNAL TABLE IF NOT EXISTS nyse_stg(
+    ticker VARCHAR(255),
+    tradedate INT,
+    openprice FLOAT,
+    highprice FLOAT,
+    lowprice FLOAT,
+    closeprice FLOAT,
+    volume BIGINT
+) USING CSV
+OPTIONS (
+    path='/user/${USERNAME}/nyse_data'
+);
