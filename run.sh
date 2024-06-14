bd288ad7e8e9563ec0b2691f774ec144897f2b5c
Lashmanbala <lashmanbala@gmail.com>
2024-06-14 13:02:28 +0000

    Downloading packages for delta and configurations for delta

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-spark-sql --conf spark.sql.warehouse.dir=/user/spark/warehouse 
+spark-sql --conf spark.sql.warehouse.dir=/user/spark/warehouse \
+  --packages io.delta:delta-spark_2.12:3.1.0 \
+  --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
+  --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"  
