30dc49d977863bd2f09d1bbb54f824cfb59c86c6
Lashmanbala <lashmanbala@gmail.com>
2024-06-14 13:05:51 +0000

    run with runtime variable

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 spark-sql --conf spark.sql.warehouse.dir=/user/spark/warehouse \
   --packages io.delta:delta-spark_2.12:3.1.0 \
   --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
-  --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"  
+  --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" \
+  -f /home/hadoop/emr/app.sql \
+  -d USERNAME=`whoami` \
+  --verbose 
